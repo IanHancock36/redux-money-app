9261534bd213f4168ac1993b844f97dd8e8ee38c
Ian Hancock <northroadian@gmail.com>
2021-06-13 11:27:37 -0500

    redux thunk

--- src/state/store.js
+++ src/state/store.js
@@ -1,9 +1,11 @@
-import {createStore} from 'redux'
+import {createStore ,applyMiddleware} from 'redux'
 import reducers from './reducers/index'
+import thunk from 'redux-thunk'
 
 export const store = createStore(
     reducers, 
-    {}
+    {},
+    applyMiddleware(thunk)
 )
 
 
